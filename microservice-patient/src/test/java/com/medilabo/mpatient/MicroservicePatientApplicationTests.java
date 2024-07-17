372771ee0ff150480c3aba542e5897618e4c204f
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-07-17 11:50:24 +0200

    Adding 'Patient' microservice subdirectory

---
+++ microservice-patient/src/test/java/com/medilabo/mpatient/MicroservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.mpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
