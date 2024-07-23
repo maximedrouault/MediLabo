8569abc9f8531b84b40240bae3816386ff36334e
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-07-23 17:48:15 +0200

    Adding eureka discovery-server

---
+++ discovery-server/src/test/java/com/medilabo/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medilabo.discoveryserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test is intentionally left blank to demonstrate the application context loading without errors.
+        // In a real-world scenario, you would add specific context-related assertions here.
+    }
+}
