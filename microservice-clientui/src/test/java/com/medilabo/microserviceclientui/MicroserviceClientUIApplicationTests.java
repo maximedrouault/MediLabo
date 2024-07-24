12612f1f28b9148407f21d444fb00a4df8df93f1
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-07-24 13:19:31 +0200

    Adding basic microservice-clientui

---
+++ microservice-clientui/src/test/java/com/medilabo/microserviceclientui/MicroserviceClientUIApplicationTests.java
@@ -0,0 +1,17 @@
+package com.medilabo.microserviceclientui;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class MicroserviceClientUIApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test is intentionally left blank to demonstrate the application context loading without errors.
+        // In a real-world scenario, you would add specific context-related assertions here.
+    }
+
+}
